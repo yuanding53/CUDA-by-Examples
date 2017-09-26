7ce88772acebce404c84aa481bac3dbaeee8085e
yuanding53 <32296267+yuanding53@users.noreply.github.com>
2017-09-26 19:36:28 +0800

    first time add something

---
+++ c=a+b/c=a+b/c=a+b/kernel.cu
@@ -0,0 +1,21 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <iostream>
+#include <stdio.h>
+
+__global__ void add(int a, int b, int *c)
+{
+	*c = a + b;
+}
+
+int main(void) {
+	int c;
+	int *dev_c;
+	cudaMalloc((void**)&dev_c, sizeof(int));
+	add<<<1, 1>>>(2, 7, dev_c);
+	cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
+	printf("2+7=%d\n", c);
+	cudaFree(dev_c);
+	return 0;
+}
